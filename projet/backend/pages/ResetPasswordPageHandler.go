bade6b6f1e20e043a98dcd08dd1a86bd5b8f8134
Maxime Cordonnier <mcordonn9277@gmail.com>
2025-03-18 13:28:08 +0100

    push to switch computer

--- projet/backend/pages/ResetPasswordPageHandler.go
+++ projet/backend/pages/ResetPasswordPageHandler.go
@@ -1,5 +1,20 @@
 package pages
 
-func ResetPasswordPage() {
-	// TODO : Display the reset password page
+import (
+	f "GoForum/functions"
+	"net/http"
+)
+
+func ResetPasswordPage(w http.ResponseWriter, r *http.Request) {
+	PageInfo := f.NewContentInterface("home", w, r)
+	// Check the user rights
+	f.GiveUserHisRights(&PageInfo, r)
+	if PageInfo["IsAuthenticated"].(bool) {
+		f.InfoPrintf("Home page accessed at %s by %s : %s\n", f.GetIP(r), f.GetUserRankString(r), f.GetUserEmail(r))
+	} else {
+		f.InfoPrintf("Home page accessed at %s\n", f.GetIP(r))
+	}
+
+	PageInfo["bareboneBase"] = true // This is a barebone page (no header or useless stuff)
+
 }
